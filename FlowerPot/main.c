36b4f20bf33048100efc7c160e66797d0d37e138
shlum1 <shlum@gmx.net>
2020-09-17 17:54:15 +0200

    geht schon recht gut, aber Erkennung von Wertänderungen funzt nicht

--- FlowerPot/main.c
+++ FlowerPot/main.c
@@ -36,10 +36,14 @@
 #define CH_AMOUNT   0
 #define CH_INTERVAL 1
 
-#define MIN_AMOUNT       1
-#define MAX_AMOUNT     999
-#define MIN_INTERVAL    10
-#define MAX_INTERVAL    70
+#define MIN_AMOUNT      10 // min. 10 sec.
+#define MAX_AMOUNT     999 // max. 1000 sec. 
+#define MIN_INTERVAL    1*24*60   // min. 1 day
+#define MAX_INTERVAL    7*24*60   // max. 7 days
+
+#define MAX_FILTER_ADC 16383.0  // max. value of 16x filtered ADC (0x3FFF)
+
+#define SHOW_CHANGE_SEC 30  // show last change for x seconds
 
 #define CFG_DDRA 0
 #define CFG_DDRB ( BIT(PO_LED_BLUE) | BIT(PO_LED_RED) | BIT(PO_LED_WHITE) | BIT(PO_LED_GREEN) | BIT(PO_DISPL_DTA) | BIT(PO_DISPL_CLK) )
@@ -48,7 +52,7 @@
 
   //Konfiguration der Pull-Ups für Eingänge
 #define CFG_PORTA 0
-#define CFG_PORTB BIT(PI_DISPL_BTN)  //pullup for button
+#define CFG_PORTB BIT(PI_DISPL_BTN)  //pull-up for button
 #define CFG_PORTC 0
 #define CFG_PORTD 0
 
@@ -56,20 +60,20 @@
 
 
 
-#define ADMUX_PRESET  ( BIT(REFS0) | BIT(ADLAR) )  // preset of ADMUX register to left align ADC result and use only 8 MSB, VACC as VREF
+#define ADMUX_PRESET  ( BIT(REFS0) /*| BIT(ADLAR)*/ )  // preset of ADMUX register to left align ADC result and use only 8 MSB, VACC as VREF
 
 
 
 void Init();
 void ShowStart();
 void SendDisplayData(uint8_t dta[5]);
-void DisplayBCD(uint8_t bcd[3]);
-void DisplayInt(int16_t i);
+void DisplayBCD(uint8_t bcd[3], uint8_t leadingZeros);
+void DisplayInt(int16_t i, uint8_t leadingZeros);
 
 void StartPump();
 void StopPump();
 uint8_t CheckWater();
-void SchowPumping();
+void SchowStatus();
 uint16_t GetAmountADC();
 uint16_t GetIntervalADC();
 
@@ -79,16 +83,19 @@ const uint8_t lookUpDigi1[16];
 
 
 
-volatile uint8_t gAdcVal[2]={0};   // filled automatically in ADC ISR
-
+volatile int16_t gAdcVal[2]={0};   // filled automatically in ADC ISR
+volatile int16_t gLastADC[2]={0};  // store last values to detect changes
 
 uint16_t gX=1;
-volatile uint16_t gPumpOnSeconds=0;    // pump seconds ON time (10..10000); set when gIrrigateInrerval is 0 (according to AIN_POTI_AMOUNT); clear in TIM1 ISR
-volatile uint16_t gHours=0;            //used in TIM1 ISR (0..(24x7)-1)
-volatile uint8_t  gIrrigateInrerval=0; //(according to AIN_POTI_INTERVAL);
+volatile uint16_t gPumpOnSeconds=0, gPumpOnPreset=0;    // pump seconds ON time (10..10000); set when gIrrigateInrerval is 0 (according to AIN_POTI_AMOUNT); clear in TIM1 ISR
+volatile uint16_t gIrrigateInterval=0, gIntervalPreset=0; // interval between irrigations  (minutes; max. 7 days * 24h * 60min == 10.080
 
-typedef enum {stInit, stIdle, stPumping} TStatus;
-TStatus gSatus;
+typedef enum {stIdle, stChanging, stWaiting, stPumping, stError} TStatus;
+TStatus gSatus=stIdle, gLastStat=stIdle;
+
+
+uint8_t gChangeChan=CH_AMOUNT; 
+volatile uint8_t gChangeSec=0;  // show the change status for some seconds
 
 
 int main(void)
@@ -97,9 +104,12 @@ int main(void)
 
    Init();
 
+   DisplayInt(123,1);
 
    ShowStart();
+   SET_BIT(PORTB, PO_LED_RED);  // clear ERR LED
 
+   SET_BIT(ADCSRA, ADSC);  // start first conversion
 
 
    //  uart_init(UART_BAUD_SELECT(19200, F_CPU));
@@ -109,7 +119,10 @@ int main(void)
    uart_puts_P("Build on " __DATE__ " at " __TIME__"\r\n");
    uart_write_P("\r\n UBRRL=%u, MCUCSR=0x%02x\r\n", UBRRL, MCUCSR);
 
-   SET_BIT(ADCSRA, ADSC);  // start first conversion
+   _delay_ms(100);
+   gLastStat=gSatus=stIdle;
+
+   DisplayInt(234,1);
 
    while(1)
    {
@@ -119,30 +132,55 @@ int main(void)
          lastBtn = x;
          if(lastBtn==0)  // button is pressed
          {
-            TGL_BIT(PORTB, PO_LED_WHITE);
-            gX *= 2;
+            SET_BIT(PORTB, PO_LED_RED);  // clear ERR LED
+            gPumpOnPreset=GetAmountADC();
+            gIntervalPreset=GetIntervalADC();
+            StartPump();
          }
 
       }
 
     //  DisplayInt(GetIntervalADC());
-    SchowPumping();
+      SchowStatus();
 
       if(CheckWater())
-        CLR_BIT(PORTB, PO_LED_RED);
+        CLR_BIT(PORTB, PO_LED_GREEN);
       else
-        SET_BIT(PORTB, PO_LED_RED);
+      {
+         SET_BIT(PORTB, PO_LED_GREEN);
+         StopPump();
+         gSatus=stError;
+      }
 
+      for(uint8_t c=0; c<2; c++)
+      {
+         if ( abs(gAdcVal[c]-gLastADC[c]) > 300 )  // consider this a change on purpose, not just noise
+         {
+            if(gSatus != stChanging)
+               gLastStat=gSatus;  // store status to recover after changing
+
+            gSatus=stChanging;
+            gChangeChan=c;
+            gChangeSec=SHOW_CHANGE_SEC;
+            uart_write_P("\r\nch=%u, AdcVal[ch]=%i, LastADC[ch]=%i, diff=%i", c, gAdcVal[c], gLastADC[c], abs(gAdcVal[c]-gLastADC[c]));
+         }
+      }
 
       if(++loop == 10)
       {
          loop=0;
 
-         uart_write_P("\r\nADC: CH0=%u, ch1=%u", gAdcVal[0], gAdcVal[1]);
+         uart_write_P("\r\nSatus: %i, ADC: Ch0=%u, Ch1=%u, PumpOnSeconds = %i sec, IrrigateInterval = %i min, ChangeSec = %i sec", 
+             gSatus, gAdcVal[0], gAdcVal[1], gPumpOnSeconds, gIrrigateInterval, gChangeSec);
+
+   
+         
+         
          TGL_BIT(PORTB, PO_LED_BLUE);
-         TGL_BIT(PORTD, PO_PUMP1);
       }
-      _delay_ms(200);
+
+      _delay_ms(100);
+
    }  // while(1)
 }  // main()
 
@@ -166,7 +204,7 @@ void Init(void)
   PORTC=CFG_PORTC;
   PORTD=CFG_PORTD;
 
-  // watchdag
+  // watchdog
  // wdt_reset();
  // WDTCSR=BIT(WDCE) | BIT(WDE); //enable chaneg first
 //  WDTCSR=BIT(WDIE) | BIT(WDE) | BIT(WDP3) | BIT(WDP0); // enable Watchdog, timeout 8sec
@@ -215,10 +253,10 @@ void ShowStart()
 
 
 //-------------------------------------------------------------------------------------------
-//  void DisplayInt(int16_t i)
+//  void DisplayInt(int16_t i, uint8_t leadingZeros=1)
 //
 //-------------------------------------------------------------------------------------------
-void DisplayInt(int16_t i)
+void DisplayInt(int16_t i, uint8_t leadingZeros)
 {
    uint8_t bcd[3];
 
@@ -226,25 +264,29 @@ void DisplayInt(int16_t i)
    bcd[1] = (i % 100) / 10;
    bcd[0] = i / 100;
 
-   DisplayBCD(bcd);
+   DisplayBCD(bcd, leadingZeros);
 }
 
 
 
 //-------------------------------------------------------------------------------------------
-//  void DisplayBCD(uint8_t bcd[3])
+//  void DisplayBCD(uint8_t bcd[3], uint8_t leadingZeros=1)
 //
 //-------------------------------------------------------------------------------------------
-void DisplayBCD(uint8_t bcd[3])
+void DisplayBCD(uint8_t bcd[3], uint8_t leadingZeros)
 {
    uint8_t data[5]={0};  // 40 bit (35 will be needed
 
-//Digit 1
-   data[0] = lookUpDigi1[bcd[0]] ;
+//Digit 1 (MSB)
+   if(leadingZeros || bcd[0])
+      data[0] = lookUpDigi1[bcd[0]] ;
 // Digit 2
-   data[1] = lookUpDigi1[bcd[1]] << 2;
-   data[2] = (lookUpDigi1[bcd[1]] >> 2) & 0x1C;
-//Digit 3
+   if(leadingZeros || bcd[0]  || bcd[1])
+   {
+      data[1] = lookUpDigi1[bcd[1]] << 2;
+      data[2] = (lookUpDigi1[bcd[1]] >> 2) & 0x1C;
+   }
+//Digit 3 (LSB)
    data[2] |= (lookUpDigi1[bcd[2]] << 5) ;
    data[3] = (lookUpDigi1[bcd[2]] & 0x08) >> 2 ;
    data[3] |= (lookUpDigi1[bcd[2]] & 0x70);
@@ -258,9 +300,9 @@ void DisplayBCD(uint8_t bcd[3])
 
 #define CLOCK(){\
    SET_BIT(PORTB, PO_DISPL_CLK);\
-   _delay_us(1);\
+   _delay_us(2);\
    CLR_BIT(PORTB, PO_DISPL_CLK);\
-   _delay_us(1);\
+   _delay_us(2);\
 }
 
 
@@ -273,6 +315,9 @@ void DisplayBCD(uint8_t bcd[3])
 void SendDisplayData(uint8_t dta[5])
 {
   uint8_t clk, byte, bit;
+
+  cli();
+
   SET_BIT(PORTB, PO_DISPL_DTA);
 
   CLOCK();  //first of 36 clock ticks
@@ -289,6 +334,7 @@ void SendDisplayData(uint8_t dta[5])
     else
        CLR_BIT(PORTB, PO_DISPL_DTA);
   }
+  sei();
 }
 
 
@@ -298,7 +344,11 @@ void SendDisplayData(uint8_t dta[5])
 //-------------------------------------------------------------------------------------------
 void StartPump()
 {
-
+   gSatus=stPumping;
+   CLR_BIT(PORTB, PO_LED_WHITE);
+   SET_BIT(PORTD, PO_PUMP1);
+   gPumpOnSeconds=gPumpOnPreset;  // set amount timeout
+   uart_write_P("\r\n Start Pump, on-time = %i sec. \r\n", gPumpOnSeconds);
 }
 
 
@@ -307,8 +357,22 @@ void StartPump()
 //
 //-------------------------------------------------------------------------------------------
 void StopPump()
-{
+{  
+   float x;
+   uint16_t f, i;
 
+   SET_BIT(PORTB, PO_LED_WHITE);
+   CLR_BIT(PORTD, PO_PUMP1);
+
+   if(gSatus==stPumping)
+   {
+      gSatus=stWaiting;
+      gIrrigateInterval=gIntervalPreset / 1000;  // set interval timeout
+      x = gIrrigateInterval / 60.0 / 24.0 * 1000;
+      i= trunc(x);
+      f=(uint16_t)((x-i)*1000.0);
+      uart_write_P("\r\n Stop Pump, next irrigation starts in %i minutes (%d.%d days)\r\n", gIrrigateInterval, i, f);
+   }
 }
 
 
@@ -343,24 +407,63 @@ uint8_t CheckWater()
 
 
 //-------------------------------------------------------------------------------------------
-//  void SchowPumping()
+//  void SchowStatus()
 //
 //-------------------------------------------------------------------------------------------
-void SchowPumping()
+void SchowStatus()
 {
    const uint8_t lookUpPumpAnimation[4][3]={{0b00000100,0b00000000,0b00000010},{0b00100000,0b00100000,0b00000000},{0b00000000,0b01000100,0b00000000},{0b00000000,0b10001000,0b00000000}};
    static uint8_t seg=0;
-   uint8_t data[5];  // 40 bit (35 will be needed
+   uint16_t i;
+   uint8_t data[5]={0};  // 40 bits (35 bits are needed)
 
-    data[0] = lookUpDigi1[seg];
-    data[1] = lookUpPumpAnimation[seg][0];
-    data[2] = lookUpPumpAnimation[seg][1];
-    data[3] = lookUpPumpAnimation[seg][2];
+   switch(gSatus)
+   {
+      case stIdle: 
+               DisplayInt(0, 1); 
+               break;
 
-    SendDisplayData(data);
+      case stChanging:
+               if(gChangeChan == CH_AMOUNT) 
+                  i = GetAmountADC();   // seconds
+               else
+                  i = GetIntervalADC() / 10;// show 10min. steps
+               
+               if (i>999)
+                  i=999;
+               
+               DisplayInt(i, 1); 
+               break;
 
-    if(++seg == 4)
-      seg=0;
+
+      case stWaiting: 
+               i = gIrrigateInterval / 10;   // show 10min. steps
+               if (i>999)
+                  i=999;
+               DisplayInt(i, 0); 
+               break;
+
+      case stPumping:
+               i = 10 * gPumpOnSeconds / (gPumpOnPreset+1);
+               data[0] = lookUpDigi1[i];
+               data[1] = lookUpPumpAnimation[seg][0];
+               data[2] = lookUpPumpAnimation[seg][1];
+               data[3] = lookUpPumpAnimation[seg][2];
+               SendDisplayData(data);
+
+               if(++seg == 4)
+                  seg=0;
+               break;
+
+      case stError: 
+               CLR_BIT(PORTB, PO_LED_RED); 
+               data[0] = 0b01111001;  // 'E'
+               data[1] = 0b00000000;  // nix
+               data[2] = 0b00010100;  // 'r'
+               data[3] = 0b01010000;  // 'r'
+               SendDisplayData(data);
+               break;
+   }  // switch
 }
 
 
@@ -370,8 +473,12 @@ void SchowPumping()
 //-------------------------------------------------------------------------------------------
 uint16_t GetAmountADC()
 {
-   float v = (float) gAdcVal[CH_AMOUNT] * (MAX_AMOUNT-MIN_AMOUNT) / 255.0 +MIN_AMOUNT;
-   return (uint16_t) v;
+//   float v = (float) gAdcVal[CH_AMOUNT] * (MAX_AMOUNT-MIN_AMOUNT) / MAX_FILTER_ADC +MIN_AMOUNT;
+
+
+   float v = (float)(gAdcVal[CH_AMOUNT]) * 2.0 / MAX_FILTER_ADC + 1.0;  // 1..3
+
+   return (uint16_t) pow(10.0, v);  // 10 .. 1000
 }
 
 
@@ -384,7 +491,7 @@ uint16_t GetAmountADC()
 //-------------------------------------------------------------------------------------------
 uint16_t GetIntervalADC()
 {
-   float v = (float) gAdcVal[CH_INTERVAL] * (MAX_INTERVAL-MIN_INTERVAL) / 255.0 + MIN_INTERVAL;
+   float v = (float) gAdcVal[CH_INTERVAL] * (MAX_INTERVAL-MIN_INTERVAL) / MAX_FILTER_ADC + MIN_INTERVAL;
    return (uint16_t) v;
 }
 
@@ -394,26 +501,39 @@ uint16_t GetIntervalADC()
 
 //-------------------------------------------------------------------------------------------
 //  ISR(TIMER1_COMPA_vect)
-//
+//  once a second
 //-------------------------------------------------------------------------------------------
 ISR(TIMER1_COMPA_vect)
 {
-   static uint16_t seconds=0;
+   static uint8_t seconds=0;
+
+   if(gSatus==stChanging)
+   {
+      gChangeSec--;
+      if(gChangeSec==0)
+         gSatus=gLastStat;
+   }
+   
+   if(gSatus==stPumping)
+   {
+      gPumpOnSeconds--;
+      if(gPumpOnSeconds==0)
+         StopPump();
+   }
 
    seconds++;
-
-   if(seconds==3600)
+   if(seconds==60)
    {
-      gHours++;
       seconds=0;
+
+      if(gSatus==stWaiting)
+      {
+         gIrrigateInterval--;  // these are minutes
+         if(gIrrigateInterval==0)
+            StartPump();
+      }
    }
-
-   if(gPumpOnSeconds > 0)
-   {
-
-   }
-
-   TGL_BIT(PORTB, PO_LED_GREEN);
+ //  TGL_BIT(PORTB, PO_LED_GREEN);
 }
 
 
@@ -428,8 +548,12 @@ ISR(TIMER1_COMPA_vect)
 ISR(ADC_vect)
 {
   static uint8_t ch=0;
+   
+  gLastADC[ch]=gAdcVal[ch];
 
-  gAdcVal[ch]=ADCH; //ignore lower 2 bit;
+  gAdcVal[ch] -= gAdcVal[ch] / 16;  // this will act as a digital filter 1/16
+  gAdcVal[ch] += ADC;
+
   if(++ch == 2)
     ch=0;
 
--- FlowerPot/util.h
+++ FlowerPot/util.h
@@ -38,6 +38,7 @@
 
   #define min(x,y) ((x)<(y)?(x):(y))
   #define max(x,y) ((x)>(y)?(x):(y))
+  #define abs(x)   ((x)<0?-(x):(x))
 
   // union erlaubt einen effektiven, separaten Zugriff auf Teile der Variable
   typedef union
